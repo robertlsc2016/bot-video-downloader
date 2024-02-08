954a590da49f3621995b096ea2ffd69fc2933a68
Robert Luiz <robertlsc2016@gmail.com>
2024-02-08 19:01:43 -0300

    fix: adjustment validation string and string qrcode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const qrcode = require('qrcode-terminal');
+
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
 const client = new Client({
@@ -14,11 +15,17 @@ const client = new Client({
     authStrategy: new LocalAuth({
         dataPath: 'auth'
     })
-});
+})
 
-const regexURLYoutube = /(https?:\/\/(www\.)?youtu(be\.com\/watch\?v=|be\/|com\/shorts\/)|[^\s]+)/i;
+const regexURL = /(https?:\/\/[^\s]+)/;
+
+const urlsYT = ["youtube.com", "youtube.com/shorts", "youtube.com/watch", "youtu.be"]
+
+
 
 client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+
     console.log(qr)
 });
 
@@ -28,8 +35,11 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
     const bruteMessageWithLink = message.body
+    console.log(bruteMessageWithLink)
+    //console.log("1", bruteMessageWithLink.match(regexURL))
+    //if (urlsYT.filter(yt => bruteMessageWithLink.includes(yt))[0]) { console.log("verdade porra") }
 
-    if (bruteMessageWithLink.match(regexURL) && (bruteMessageWithLink.match(regexURLYoutube))) {
+    if (bruteMessageWithLink.match(regexURL) && urlsYT.filter(yt => bruteMessageWithLink.includes(yt))[0]) {
         const cleanLink = bruteMessageWithLink.match(regexURL)[0]
         console.log(cleanLink)
         downloadVDYoutube(message, cleanLink)
@@ -38,6 +48,7 @@ client.on('message', async (message) => {
 });
 
 const downloadVDYoutube = async (message, cleanLink) => {
+    console.log('entroiu no downloadVDYoutube')
     let writableStream
 
     if (ytdl.validateURL(cleanLink)) {
@@ -45,14 +56,21 @@ const downloadVDYoutube = async (message, cleanLink) => {
             .pipe(writableStream = fs.createWriteStream('video.mp4'));
     }
 
-    writableStream.on('finish', () => {
-        try {
+    writableStream.on('finish', () => 
+    {
+        try 
+        {
+            console.log("tentando")
             const media = MessageMedia.fromFilePath('./video.mp4');
             client.sendMessage(message.from, media, { caption: "Ta ai o vídeo do link que tu mandou, corno" })
-        } catch (err) {
+        } 
+        catch (err) 
+        {
             console.error('Ocorreu um erro:', err);
         }
     });
+
+
 }
 
 
