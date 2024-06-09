8999207f9818f050f23bad052005f7c50f3eab63
Robert Luiz <robertlsc2016@gmail.com>
2024-06-09 14:26:01 -0300

    feat: support for downloading videos from facebook full

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
 const os = require("os");
+const path = require("path");
+const { exec } = require("child_process");
+
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+// const ffmpeg = require("fluent-ffmpeg");
 
 const ytdl = require("ytdl-core");
 const getFbVideoInfo = require("fb-downloader-scrapper");
@@ -15,37 +23,38 @@ const OS =
     ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
     : "/usr/bin/google-chrome-stable";
 
+// const inputPathRoot = path.join(__dirname, "\\videos\\facebook-video.mp4"); // Caminho do vídeo de entrada
+// const outputPathRoot = path.join(__dirname, "\\videos\\clear-facebook-video.mp4"); // Caminho do vídeo de saída
+
+const inputPathRoot = path.join(".\\", "videos\\facebook-video.mp4"); // Caminho do vídeo de entrada
+const outputPathRoot = path.join(".\\", "videos\\clear-facebook-video.mp4"); // Caminho do vídeo de saída
+
+const webversion = "2.2412.54v2"
+// const webversion = "2.2413.51-beta-alt";
+
 const client = new Client({
   puppeteer: {
     // args: ["--disable-setuid-sandbox", "--disable-gpu", "--no-sandbox"],
     args: ["--no-sandbox", "--disable-gpu"],
 
     headless: true,
+    // executablePath: '.\\chrome\\win64-125.0.6422.141\\chrome-win64\\chrome.exe'
     executablePath:
-      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+      "C:\\Users\\Robert Luiz\\AppData\\Local\\Programs\\Opera\\opera.exe",
+    // executablePath:
+    //   "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
   },
   authStrategy: new LocalAuth({
     dataPath: "auth",
   }),
-  // webVersionCache: {
-  //   type: "remote",
-  //   remotePath:
-  //     "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-  // },
 
-  // webVersion: "2.2412.54v2",
+  webVersion: webversion,
   webVersionCache: {
     type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2412.54v2.html",
+    // path: `./html/${webversion}.html`,
+    remotePath: `https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/${webversion}.html`
   },
-
-  // webVersionCache: {
-  //   type: "remote",
-  //   remotePath:
-  //     "https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2412.54v2.html",
-  // },
-  // restartOnAuthFail: true,
+  restartOnAuthFail: true,
 });
 
 const regexURL = /(https?:\/\/[^\s]+)/;
@@ -91,22 +100,26 @@ client.on("message", async (message) => {
 
 const downloadVDFacebook = async (url, message) => {
   try {
+    console.log("estrou aq");
     const filePath = "facebook-video.mp4";
 
     await getFbVideoInfo(url).then(async (result) => {
-      const fileStream = fs.createWriteStream(`.\\videos\\${filePath}`);
+      // const fileStream = fs.createWriteStream(`.\\videos\\${filePath}`);
 
       await axios({
         method: "get",
-        url: result.sd,
+        url: result.hd,
         responseType: "stream",
       }).then(async (response) => {
         const writer = fs.createWriteStream(`.\\videos\\${filePath}`);
-
         await response.data.pipe(writer);
 
-        writer.on("finish", () => {
-          sendVideo(message, filePath);
+        writer.on("finish", async () => {
+          try {
+            await cleanVideo(`.\\videos\\${filePath}`, message);
+          } catch (error) {
+            console.error("Erro ao limpar o vídeo ou enviar o vídeo:", error);
+          }
         });
       });
     });
@@ -144,9 +157,41 @@ const downloadVDYoutube = async (message, cleanLink) => {
 const sendVideo = async (message, filePath) => {
   const media = MessageMedia.fromFilePath(`.\\videos\\${filePath}`);
 
-  client.sendMessage(message.from, media, {
-    caption: "Ta ai o vídeo do link que tu mandou, corno",
-  });
+  try {
+    client.sendMessage(message.from, media, {
+      caption: "Ta ai o vídeo do link que tu mandou, corno",
+      sendMediaAsDocument: false,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+async function cleanVideo(inputPathRoot, message) {
+  ffmpeg(inputPathRoot)
+    .outputOptions([
+      "-c:v copy", // Copia o vídeo sem reencodar
+      "-c:a copy", // Copia o áudio sem reencodar
+    ])
+    .on("start", function (commandLine) {
+      console.log("Iniciado o processamento com comando: " + commandLine);
+    })
+    .on("progress", function (progress) {
+      console.log("Processando: " + progress.percent + "% concluído");
+    })
+    .on("end", function async() {
+      console.log("Processamento finalizado com sucesso!");
+
+      try {
+        sendVideo(message, "clear-facebook-video.mp4");
+      } catch (err) {
+        console.log(err);
+      }
+    })
+    .on("error", function (err, stdout, stderr) {
+      console.log("Erro durante o processamento: " + err.message);
+    })
+    .save(outputPathRoot);
+}
 
 client.initialize();
