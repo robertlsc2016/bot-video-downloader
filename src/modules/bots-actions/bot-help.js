71734253ac04f27954ab4519cf5150621120731c
Robert Luiz <robertlsc2016@gmail.com>
2024-06-28 23:02:37 -0300

    fix: adjusted message bot help

--- src/modules/bots-actions/bot-help.js
+++ src/modules/bots-actions/bot-help.js
@@ -6,6 +6,6 @@ const {
 module.exports.bothelp = async function ({ from: from }) {
   await genericSendMessageOrchestrator({
     type: "text",
-    msg: `Minhas funcionalidades atuais são:\n1. Posso baixar videos de Tiktok, Instagram, Facebook, Twitter e X(Ex-Twitter) basta enviar a URL do vídeo\n2. Posso jogar cara ou coroa, basta digita exatamente: *${prefixBot} cara ou coroa*\n3. Posso transformar imagem em sticker, basta mandar a imagem com a descrição *${prefixBot} sticker*\n4. Posso marcar alguém aleatoriamente do grupo, basta digitar *${prefixBot} quem é* ... \n5. Posso dizer se algo é verdade ou não, basta digitar *${prefixBot} é verdade* ... ?`,
+    msg: `Minhas funcionalidades atuais são:\n1. Posso baixar videos de Tiktok, Instagram, Facebook, Twitter e X(Ex-Twitter) basta enviar a URL do vídeo\n2. Posso jogar cara ou coroa, basta digita exatamente: *${prefixBot} cara ou coroa*\n3. Posso transformar imagem em sticker, basta mandar a imagem com a descrição *${prefixBot} sticker*\n4. Posso marcar alguém aleatoriamente do grupo, basta digitar *${prefixBot} quem é* ... \n5. Posso dizer se algo é verdade ou não, basta digitar *${prefixBot} é verdade* ... ?\n6. Posso converter uma mensagem grande em áudio. Ela precisa ter mais de 250 caracteres`,
   });
 };
