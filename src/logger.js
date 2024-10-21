178d8ac5e9f7d0ea6214e6d53d92c803854cafcd
Dercilio <38219914+GGontijo@users.noreply.github.com>
2024-10-21 09:05:34 -0400

    feat:  propper log format

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,15 @@
 const winston = require("winston");
+const { combine, timestamp, printf, colorize, align } = winston.format;
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple()
+  format: combine(
+    colorize({ all: true }),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+    }),
+    align(),
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
   ),
   transports: [new winston.transports.Console()],
 });
